bf21d142a77ccf31925676163d703f344b7d6dbd
pi-chan <hiromasa.ohno.1007@gmail.com>
2020-02-21 08:33:57 +0900

    とりあえず素で通せるように

--- projects/06/main.rb
+++ projects/06/main.rb
@@ -1,3 +1,19 @@
+require 'pry-byebug'
+
 require_relative './assembler/parser.rb'
 
-puts 'hello!'
+in_file = ARGV[0]
+if !File.exists?(in_file) || File.ftype(in_file) != 'file' || File.extname(in_file) != '.asm'
+  puts 'invalid argument'
+  exit 1
+end
+
+dir = File.dirname(in_file)
+file = File.basename(in_file, '.asm')
+out_file = File.join(dir, "#{file}.hack")
+
+File.open(out_file, 'w') do |f|
+  File.foreach(in_file) do |line|
+    f.puts line
+  end
+end
